e1c7073b18bca48006a58a1185dcb02d970e2a5a
Sandeep Kota <sandeep.kota1997@gmail.com>
2020-03-11 13:45:41 -0400

    Implementaton on Data1

--- code.py
+++ code.py
@@ -4,15 +4,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-
 def main():
 
-	# for i in 
+	cap = cv2.VideoWriter('Data1.mp4',cv2.VideoWriter_fourcc(*'MP4V'), 20.0, (1280,370))
 
 	path = '/home/default/ENPM673/Project2/Dataset/data_1/data/'
 
-	imgs = os.listdir(path)
-	# imgs = ["0000000170.png"]
+	imgs = sorted(os.listdir(path))
+ 
+	# imgs = ["0000000255.png"]
 	for i in imgs:
 		img = cv2.imread(path + str(i))
 		print(path + str(i))
@@ -29,14 +29,17 @@ def main():
 		dst = cv2.undistort(img,K,dist,None,newcameramtx)
 		# cv2.imshow("Undistorted",dst)
 
-		h_pts_img = np.array([[364,417],[622,254],[707,254],[846,417]], dtype="float32")
+		# h_pts_img = np.array([[364,417],[622,254],[707,254],[846,417]], dtype="float32")
+		h_pts_img = np.array([[364,417],[643,240],[700,240],[846,417]], dtype="float32")
 		h_pts_gt = np.array([[32,512],[32,0],[192,0],[192,512]],dtype="float32")
 		
 		H,_ = cv2.findHomography(h_pts_img, h_pts_gt)
 		# print("H Matrix: ",H)
 		
 		warped = cv2.warpPerspective(dst,H,(256,512))
+		cv2.imshow("Warped :", warped)
 
+		# cv2.imshow("canny",cv2.Canny(cv2.cvtColor(warped,cv2.COLOR_BGR2GRAY),150,200))
 		# cv2.imshow("Unidistorted :", dst)
 		gray = cv2.cvtColor(warped,cv2.COLOR_BGR2GRAY)
 		edges = np.zeros((gray.shape[0],gray.shape[1]),dtype=np.uint8)
@@ -44,7 +47,7 @@ def main():
 
 		# plt.figure("Thres")
 		# plt.imshow(edges)
-		# cv2.imshow("Thresh",edges)
+		cv2.imshow("Thresh",edges)
 
 		hist_along_x_left = np.sum(edges[:,:int(edges.shape[1]/2)]>0, axis=0)
 		hist_along_x_right = np.sum(edges[:,int(edges.shape[1]/2):]>0, axis=0)
@@ -55,7 +58,7 @@ def main():
 
 		left_lane = np.argmax(hist_along_x_left)
 		right_lane = int(edges.shape[1]/2) + np.argmax(hist_along_x_right)
-		print("my lanes:", left_lane, right_lane)
+		# print("my lanes:", left_lane, right_lane)
 
 		if left_lane-15>0:
 			edges[:, :left_lane-15] = 0
@@ -68,9 +71,12 @@ def main():
 
 		# plt.figure("New Edges")
 		# plt.imshow(edges)
-		# cv2.imshow("New Edges",edges)
+		cv2.imshow("New Edges",edges)
+		# cv2.waitKey(0)
+		# sys.exit()
 
 		# cv2.waitKey(0)
+		# plt.show()
 		# sys.exit(0)
 
 		left_lane_pts = np.where(edges[:, :left_lane+15]>0)
@@ -90,46 +96,63 @@ def main():
 		# Radius of curvature formula https://www.quora.com/How-can-I-find-the-curvature-of-parabola-y-ax-2-bx-c
 		curvature =(10**11) * ((2*pr[0])/((1+((2*pr[0]*(-pr[2]/(2*pr[0])))+pr[1])**2)*np.sqrt(1+((2*pr[0]*(-pr[2]/(2*pr[0])))+pr[1])**2)))
 		curve = "Straight"
-		if (curvature>(0.6)):
+		if (curvature>(0.8)):
 			curve = "Right"
-		if (curvature<-0.6):
+		if (curvature<-0.7):
 			curve = "Left"		
 
 		# print("Polyfit Left: ",pl)
 		# print("Polyfit Right: ",pr)
 		# print("Curvature :",curvature )
-		# right_poly = []
-		# left_poly = []
 		lane = np.zeros((warped.shape[0],warped.shape[1],2))
 		for x in range(0,512):
 			# print("Value :",x,((pl[0]*(x**2)) + (pl[1]*(x**1)) + (pl[2])),((pr[0]*(x**2)) + (pr[1]*(x**1)) + (pr[2])))
-			# warped[x,int((pl[0]*(x**2)) + (pl[1]*(x**1)) + (pl[2]))] = [255,0,0]
-			# warped[x,int((pr[0]*(x**2)) + (pr[1]*(x**1)) + (pr[2]))] = [0,0,255]
-			lane[x,int((pl[0]*(x**2)) + (pl[1]*(x**1)) + (pl[2])),0] = 255
+			if (((pl[0]*(x**2)) + (pl[1]*(x**1)) + (pl[2])) < 256) and (((pl[0]*(x**2)) + (pl[1]*(x**1)) + (pl[2])) > 0):
+				lane[x,int((pl[0]*(x**2)) + (pl[1]*(x**1)) + (pl[2])),0] = 255
 			lane[x,int((pr[0]*(x**2)) + (pr[1]*(x**1)) + (pr[2])),1] = 255
-			# right_poly.append([x,int((pl[0]*(x**2)) + (pl[1]*(x**1)) + (pl[2]))])
-			# left_poly.append([x,int((pr[0]*(x**2)) + (pr[1]*(x**1)) + (pr[2]))])
 
-		# right_poly = np.array(right_poly)
-		# left_poly = np.array(left_poly)
-
-		print("Curve :",curve)
-		# plt.figure("Warped")
-		# cv2.imshow("Warped",warped)
-		# cv2.imshow("Lane l",lane[:,:,0])
-		# cv2.imshow("Lane r",lane[:,:,1])
-				
 		world_lane_left = cv2.warpPerspective(lane[:,:,0],np.linalg.inv(H),(w,h))
 		world_lane_right = cv2.warpPerspective(lane[:,:,1],np.linalg.inv(H),(w,h))
 
 		dst[world_lane_left>0] = [255,0,0]
 		dst[world_lane_right>0] = [0,0,255]
-		print([world_lane_left>0,world_lane_right>0])
-		# world_lane = cv2.warpPerspective(warped,np.linalg.inv(H),(w,h))
-		# dst = cv2.fillPoly(dst,[world_lane_left>0,world_lane_right>0],[0,255,0])
-		cv2.imshow("Lane World",dst)
 
-		cv2.waitKey(0)
+		# cv2.imshow("Lane Left",world_lane_left)
+		# cv2.imshow("Lane Roght",world_lane_right)
+
+		# plt.figure("Lane Left")
+		# plt.imshow(world_lane_left)
+		# plt.figure("Lane Right")
+		# plt.imshow(world_lane_right)
+		# plt.show()
+		# sys.exit(0)
+		left = np.argwhere(world_lane_left>0)
+		left = np.flip(left,axis=1)
+		right = np.argwhere(world_lane_right>0)
+		right = np.flip(right,axis=1)
+
+		for i in np.arange(left[0,0],right[0,0]):
+			top.append([i,left[0,1]])
+		top = np.array(top)
+		# print("Top",top)
+
+		bottom = []
+		for i in np.arange(left[-1,0],right[-1,0]):
+			bottom.append([i,left[-1,1]])
+		bottom = np.array(bottom)
+		# print("Bottom",bottom)
+
+		poly_lane = np.zeros((dst.shape[0],dst.shape[1],3),np.uint8 )
+		poly_lane = cv2.fillPoly(poly_lane,[np.vstack((np.flip(left,axis=0),right))],[0,255,0])
+
+		dst = cv2.addWeighted(dst,0.8,poly_lane,0.2,1)
+		dst = cv2.putText(dst,curve,(500,150),1,cv2.FONT_HERSHEY_DUPLEX,(0,0,255),2,cv2.LINE_AA)
+		cv2.imshow("Lane World",dst[60:430,50:1330])
+		cap.write(dst[60:430,50:1330])
+		# cv2.imshow("Lane ",poly_lane)
+		# cv2.waitKey(0)
+	print("Saving Video!")
+	cap.release()
 
 
 if __name__=="__main__":
